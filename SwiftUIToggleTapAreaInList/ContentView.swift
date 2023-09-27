4bac0bb6a6c6c9fb735aea7f1193f37d3adcbe83
Simon Nickel <simonnickel@googlemail.com>
2023-09-27 10:55:33 +0200

    Add project

---
+++ SwiftUIToggleTapAreaInList/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  SwiftUIToggleTapAreaInList
+//
+//  Created by Simon Nickel on 27.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	
+	@State private var isOn: Bool = true
+	@State private var selectedStyle: String = "A"
+	
+    var body: some View {
+		
+        List {
+			
+			Toggle(isOn: $isOn, label: {
+				VStack(alignment: .leading) {
+					Text("Label")
+					Text("-> Label is not tapable")
+				}
+			})
+			
+			Picker(selection: $selectedStyle) {
+				ForEach(["A", "B", "C"], id: \.self) { option in
+					Text(option)
+						.tag(option)
+				}
+			} label: {
+				VStack(alignment: .leading) {
+					Text("Picker")
+					Text("-> Lable is tapable")
+				}
+			}
+			
+        }
+
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIToggleTapAreaInList/SwiftUIToggleTapAreaInListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToggleTapAreaInListApp.swift
+//  SwiftUIToggleTapAreaInList
+//
+//  Created by Simon Nickel on 27.09.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToggleTapAreaInListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
